6f03aee371cf0074b20c4d4e8e5fb49279c67ea7
Alexander Garcia <alexg97343@gmail.com>
2020-09-22 21:17:14 -0400

    created the update and delete query they are very similiar

--- config/orm.js
+++ config/orm.js
@@ -33,6 +33,37 @@ var orm = {
         cb(res);
       });
     },
+    // An example of objColVals would be {name: panther, sleepy: true}
+  updateOne: function (table, objColVals, condition, cb) {
+    var dbQuery = 
+    'UPDATE ' + 
+    table +
+    ' SET ' +
+    translateSql(objColVals) +
+    " WHERE " +
+    condition;
+
+    console.log(dbQuery);
+    connection.query(dbQuery, function (err, res) {
+      if (err) {
+        throw err;
+      }
+
+      cb(res);
+    });
+  },
+  deleteOne: function (table, condition, cb) {
+    var dbQuery = " DELETE FROM " + table + " WHERE " + condition;
+    console.log(dbQuery);
+    connection.query(dbQuery, function (err, res) {
+      if (err) {
+        throw err;
+      }
+
+      cb(res);
+    });
+  },
+};
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
