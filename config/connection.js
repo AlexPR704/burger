9abc991551f8d8b43343026ca73936c47f107b7c
Alexander Garcia <alexg97343@gmail.com>
2020-09-22 20:48:34 -0400

    created config folder with a connection file. it is setup to connect to node to mysql

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Set up MySQL connection. requiring our dependencies
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "burgers_db"
+  });
+};
+
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
