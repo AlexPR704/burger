8e90088152a202a69e5385b3b7012c9e8f57283b
Alexander Garcia <alexg97343@gmail.com>
2020-09-22 21:26:32 -0400

    creating all of the routes and setting up the logic within those routes where required

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,30 @@ var burger = require('../models/burger');
 
 var router = express.Router();
 
+// Create all our routes and set up logic within those routes where required.
+router.get('/', function (req, res) {
+    burger.selectAll(function (data) {
+      var hdbrsObj = {
+        burgers: data,
+      };
+      console.log(hdbrsObj);
+      res.render('index', hdbrsObj);
+    });
+  });
+  
+  router.post('/api/burgers', function (req, res) {
+    burger.insertOne(
+      ['burger_name', 'devoured'],
+      [req.body.burger_name, req.body.devoured],
+      function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+      }
+    );
+  });
+  
+  
+  });
 
 
 
